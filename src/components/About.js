6f349fabce76290277a520a888dbccc1d5b5932f
hackwellbn <119762901+hackwellbn@users.noreply.github.com>
2025-04-24 12:52:20 +0000

    initial commit

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,15 @@
+// src/components/About.js
 import React from "react";
-import { image } from "../data/data";
+import { image } from "../data/data"; // Make sure this file exports an image variable
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>I love building web applications and learning new technologies.</p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
